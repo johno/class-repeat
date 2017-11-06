874dfe087c61549d0dfb1a365a97539eb0391551
Bo Jeanes <me@bjeanes.com>
2017-11-06 12:19:57 +1100

    Handle pseudo-selectors more robustly
    I'm not 100% on how all browsers will interpret repeated pseudo-selectors
    (especially pseudo-elements, as `::before::before` could be interpreted as "the
    block before the block before the element"). Simply dropping the `isPseudo`
    check or changing would still fix this issue but would be a more fundamental
    decision about what is or isn't repeated in selectors.
    
    Fixes johnotander/postcss-class-repeat#1

--- index.js
+++ index.js
@@ -16,14 +16,11 @@ module.exports = function classRepeat (selector, options) {
     var tokens = SelectorTokenizer.parse(selector).nodes[0] || { nodes: [] }
     var tokensWithRepeatedClasses = []
     tokens.nodes.map(function (node, index) {
-      if (node.type === 'class') {
+      if (node.type === 'class' || isPseudo(node)) {
         for (var i = 0; i < options.repeat; i++) {
           tokensWithRepeatedClasses.push(node)
 
           var nextToken = tokens.nodes[index + 1]
-          if (isPresent(nextToken) && isPseudo(nextToken) && i + 1 !== options.repeat) {
-            tokensWithRepeatedClasses.push(nextToken)
-          }
         }
       } else {
         tokensWithRepeatedClasses.push(node)
--- test.js
+++ test.js
@@ -2,10 +2,11 @@ import test from 'ava'
 import classRepeat from './'
 
 test('class-repeat repeats classes', t => {
-  t.plan(5)
+  t.plan(6)
 
   t.deepEqual(classRepeat('.foo.bar'), '.foo.foo.bar.bar')
-  t.deepEqual(classRepeat('.foo:before'), '.foo:before.foo:before')
+  t.deepEqual(classRepeat('.foo:hover'), '.foo.foo:hover:hover')
+  t.deepEqual(classRepeat('.foo::before'), '.foo.foo::before::before')
   t.deepEqual(classRepeat('.foo .bar.baz'), '.foo.foo .bar.bar.baz.baz')
   t.deepEqual(classRepeat('input.foo-bar.baz > .pizazz'), 'input.foo-bar.foo-bar.baz.baz > .pizazz.pizazz')
   t.deepEqual(classRepeat('.foo', { repeat: 4 }), '.foo.foo.foo.foo')
