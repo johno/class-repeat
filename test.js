e5b41b186759679189bb3296b8b96ae6e3a6104a
Bo Jeanes <me@bjeanes.com>
2017-11-06 12:03:07 +1100

    Fix tests for latest ava

--- test.js
+++ test.js
@@ -4,9 +4,9 @@ import classRepeat from './'
 test('class-repeat repeats classes', t => {
   t.plan(5)
 
-  t.same(classRepeat('.foo.bar'), '.foo.foo.bar.bar')
-  t.same(classRepeat('.foo:before'), '.foo:before.foo:before')
-  t.same(classRepeat('.foo .bar.baz'), '.foo.foo .bar.bar.baz.baz')
-  t.same(classRepeat('input.foo-bar.baz > .pizazz'), 'input.foo-bar.foo-bar.baz.baz > .pizazz.pizazz')
-  t.same(classRepeat('.foo', { repeat: 4 }), '.foo.foo.foo.foo')
+  t.deepEqual(classRepeat('.foo.bar'), '.foo.foo.bar.bar')
+  t.deepEqual(classRepeat('.foo:before'), '.foo:before.foo:before')
+  t.deepEqual(classRepeat('.foo .bar.baz'), '.foo.foo .bar.bar.baz.baz')
+  t.deepEqual(classRepeat('input.foo-bar.baz > .pizazz'), 'input.foo-bar.foo-bar.baz.baz > .pizazz.pizazz')
+  t.deepEqual(classRepeat('.foo', { repeat: 4 }), '.foo.foo.foo.foo')
 })
